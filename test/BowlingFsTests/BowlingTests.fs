b1d0dc8e1dfc1d07110d41134958aaefab1a0ab1
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2022-11-16 22:14:01 +1100

    refactor: rename test list

--- test/BowlingFsTests/BowlingTests.fs
+++ test/BowlingFsTests/BowlingTests.fs
@@ -9,15 +9,15 @@ module Gen =
     let firstBowl = Gen.choose(0, 10)
 
 [<Tests>]
-let BowlingTests = testList "bowling" [
+let ScoreTests = testList "score" [
 
-    test "new game starts with zero score" {
+    test "of a new game is zero" {
         Game.newGame
         |> Game.score
         |> Expect.equal "the score of a new game should be zero" 0
     }
 
-    testProperty "score after one bowling is the number of knocked pins"
+    testProperty "after bowling once is the amomunt of knocked pins"
         (Prop.forAll (Arb.fromGen Gen.firstBowl) <| fun knockedPins ->
             Game.newGame
             |> Game.bowl knockedPins
