184aa9f505afffe4a99ab1096234b0acc243aa69
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2022-11-17 17:20:40 +1100

    refactor: implement game as a list of frames

--- src/BowlingFs/Bowling.fs
+++ src/BowlingFs/Bowling.fs
@@ -1,10 +1,27 @@
 ﻿namespace BowlingFs
 
-type Game = Game of score:int
+type Frame =
+    | PlayingFrame of first:int
+    | OpenFrame of first:int * second:int
+
+type Game = private Game of frames:Frame list
+
+module Frame =
+    let score = function
+        | PlayingFrame firstBowlScore -> firstBowlScore
+        | OpenFrame (firstBowlScore, secondBowlScore) -> firstBowlScore + secondBowlScore
 
 module Game =
-    let newGame = Game 0
+    let newGame = Game []
 
-    let score = function Game score' -> score'
+    let private frames = function Game frames' -> frames'
 
-    let bowl knockedPins = function Game score -> Game (score + knockedPins)
+    let score = frames >> List.map Frame.score >> List.sum
+
+    let bowl knockedPins = function
+        | Game (PlayingFrame firstBowlScore :: frames) ->
+            OpenFrame (firstBowlScore, knockedPins) :: frames
+            |> Game
+        | Game frames ->
+            PlayingFrame knockedPins :: frames
+            |> Game
